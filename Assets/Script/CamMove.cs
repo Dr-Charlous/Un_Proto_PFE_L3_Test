880d4b8e16abaaced4fd72bb73fdf9bd0a9117c6
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2023-10-18 13:15:53 +0200

    Déplacement correct même si pas encore cam

--- Assets/Script/CamMove.cs
+++ Assets/Script/CamMove.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class CamMove : MonoBehaviour
 {
     public GameObject objectToRotate;
+    public bool rotatePlayer = false;
     public float distance = 5;
     public float speed = 5;
 
     [Range(0f, 360f)]
-    public float currentAngle = 0;
+    public float currentAngle = 180;
     [Range(0, 90)]
     public int currentXAngle = 45;
 
     private void Update()
     {
-        if (currentAngle >= 360)
-            currentAngle = 0;
-        currentAngle += 1 * speed * Time.deltaTime;
-        Quaternion rotation = Quaternion.Euler(currentXAngle, currentAngle, 0);
-        Vector3 position = objectToRotate.transform.position - (rotation * Vector3.forward * distance);
-        transform.position = position;
-        transform.rotation = rotation;
+        if (rotatePlayer == false)
+        {
+            if (currentAngle >= 360)
+                currentAngle = 0;
+            currentAngle += 1 * speed * Time.deltaTime;
+            Quaternion rotation = Quaternion.Euler(currentXAngle, currentAngle, 0);
+            Vector3 position = objectToRotate.transform.position - (rotation * Vector3.forward * distance);
+            transform.position = position;
+            transform.rotation = rotation;
+        }
+        else
+        {
+            Quaternion rotation = Quaternion.Euler(currentXAngle, currentAngle, objectToRotate.transform.rotation.z);
+            Vector3 position = objectToRotate.transform.position - (rotation * Vector3.forward * distance);
+            transform.position = position;
+            transform.localEulerAngles = new Vector3(currentXAngle, 180 + objectToRotate.transform.rotation.y * 100, 0);
+        }
     }
 }
--- Assets/Script/CharaMove.cs
+++ Assets/Script/CharaMove.cs
@@ -5,33 +5,34 @@ using UnityEngine;
 public class CharaMove : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float rotateSpeed = 0.5f;
     public Rigidbody _rb;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            _rb.AddForce(Vector3.forward * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            _rb.AddForce(Vector3.back * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            _rb.AddForce(Vector3.left * moveSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up * -1 * rotateSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            _rb.AddForce(Vector3.right * moveSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up * 1 * rotateSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            _rb.AddForce(Vector3.up * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
         }
     }
 }
