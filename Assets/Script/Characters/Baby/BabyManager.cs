9fde83e05e2eb9b865d9f70d856fa114d38aab8e
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-01-25 16:31:39 +0100

    dessus dessous

--- Assets/Script/Characters/Baby/BabyManager.cs
+++ Assets/Script/Characters/Baby/BabyManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class BabyManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> BabiesRef;
+    [SerializeField] List<GameObject> BabiesView;
+    [SerializeField] GameObject BabyPrefab;
     [SerializeField] Transform RespawnPoint;
     [SerializeField] BabyMove Baby;
 
@@ -35,11 +37,22 @@ public class BabyManager : MonoBehaviour
 
             BabiesRef.Add(Baby.transform.parent.gameObject);
             Baby.transform.parent.gameObject.SetActive(false);
-            Baby = null;
-        } else if (BabiesRef.Count > 0 && Baby == null)
-        {
-            BabiesRef[0].GetComponent<BabyMove>().State = BabyMove.state.Stay;
 
+            var baby = Instantiate(BabyPrefab, transform);
+            BabiesView.Add(baby);
+
+            baby.transform.rotation = Baby.transform.rotation;
+            baby.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+            Baby = null;
+
+            for (int i = 0; i < BabiesView.Count; i++)
+            {
+                BabiesView[i].transform.localPosition = Vector3.up * (i/10f - transform.position.y);
+            }
+        } 
+        else if (BabiesRef.Count > 0 && Baby == null)
+        {
             BabiesRef[0].SetActive(true);
             BabiesRef[0].GetComponentInChildren<NavMeshAgent>().destination = RespawnPoint.position;
 
@@ -48,6 +61,14 @@ public class BabyManager : MonoBehaviour
 
             BabiesRef[0].transform.position = RespawnPoint.position;
             BabiesRef.RemoveAt(0);
+
+            Destroy(BabiesView[0]);
+            BabiesView.RemoveAt(0);
+
+            for (int i = 0; i < BabiesView.Count; i++)
+            {
+                BabiesView[i].transform.localPosition = Vector3.up * (i / 10f - transform.position.y);
+            }
         }
     }
 
