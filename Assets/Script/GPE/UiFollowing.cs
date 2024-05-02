ef507f5091df6ff6e79c5fb489d4299ad842f1d3
Dr.Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-05-02 16:05:17 +0200

    [PROG] distance d'affichage icone

--- Assets/Script/GPE/UiFollowing.cs
+++ Assets/Script/GPE/UiFollowing.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class UiFollowing : MonoBehaviour
 {
     Camera _cam;
+    SpriteRenderer _render;
+    [SerializeField] CharaMove _chara;
+    [SerializeField] float _distance;
 
     private void Start()
     {
         _cam = Camera.main;
+        _render = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void Update()
     {
-        transform.LookAt(_cam.transform.position);
+        if (Vector3.Distance(_chara.transform.position, transform.position) < _distance)
+        {
+            _render.enabled = true;
+            transform.LookAt(_cam.transform.position);
+        }
+        else
+        {
+            _render.enabled = false;
+        }
     }
 }
