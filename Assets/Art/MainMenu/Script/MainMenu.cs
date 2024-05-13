41af2dc5f0397a8c75cbc48dda8daf1436f8f10e
mohimaa <mohima.islam@hotmail.com>
2024-05-13 11:09:52 +0200

    [ART] WIP Button Animation

--- Assets/Art/MainMenu/Script/MainMenu.cs
+++ Assets/Art/MainMenu/Script/MainMenu.cs
@@ -5,15 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
 
+   
     public void QuitGame()
     {
         Application.Quit();
     }
+
+
 
 }
