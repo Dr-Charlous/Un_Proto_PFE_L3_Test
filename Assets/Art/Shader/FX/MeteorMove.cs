05b7d8699417be52465ff953ac722dd10e58258a
Didinesama <mahiedine.boudrissa@gmail.com>
2024-05-17 14:40:20 +0200

    g des meteorite qui tombe et qui font le bordel

--- Assets/Art/Shader/FX/MeteorMove.cs
+++ Assets/Art/Shader/FX/MeteorMove.cs
@@ -6,6 +6,7 @@ public class MeteorMove : MonoBehaviour
 {
     public float speed;
     public GameObject impactPrefab;
+    public List<GameObject> trails;
 
     private Rigidbody rb;
 
@@ -35,6 +36,20 @@ public class MeteorMove : MonoBehaviour
         {
             var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
             Destroy( impactVFX ,5 );
+        }
+
+        if (trails.Count > 0)
+        {
+            for (int i = 0; i < trails.Count; i++)
+            {
+                trails[i].transform.parent = null;
+                var ps = trails[i].GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Stop();
+                    Destroy(ps.gameObject,ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+            }
         }
         Destroy(gameObject);
     }
