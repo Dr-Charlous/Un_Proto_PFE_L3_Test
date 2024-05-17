af3a58ec05b2b780862646fef282a250b7be069f
Didinesama <mahiedine.boudrissa@gmail.com>
2024-05-17 14:15:27 +0200

    s P092478HINPZ P0Wioh  ùp ZOetezg

---
+++ Assets/Art/Shader/FX/SpawnMeteorScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class SpawnMeteorScript : MonoBehaviour
+{
+    public GameObject vfx;
+    public Transform startPoint;
+    public Transform endPoint;
+    void Start()
+    {
+        var startPos = startPoint.position;
+        GameObject objVFX = Instantiate(vfx, startPos, Quaternion.identity) as GameObject;
+
+        var endPos = endPoint.position;
+
+        RotateTo(objVFX, endPos);
+    }
+
+    void RotateTo (GameObject obj, Vector3 destination)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+
+}
