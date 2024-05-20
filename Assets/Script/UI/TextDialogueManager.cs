79d47993ad545535bffeadb403a889a357f065e0
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-05-20 23:09:48 +0200

    ouais

--- Assets/Script/UI/TextDialogueManager.cs
+++ Assets/Script/UI/TextDialogueManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class TextDialogueManager : MonoBehaviour
 {
-    //[SerializeField] string _message = "Lorum ipsum a tes souhaits je sais pas quoi écrire aled merci c'est pour voir la taille du texte yeepee !";
     public Image UiBack;
     public TextMeshProUGUI UiText;
 }
