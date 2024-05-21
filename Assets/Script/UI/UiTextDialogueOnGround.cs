675e750a9149f74d532101d1b10d50e9925be627
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-05-21 14:05:20 +0200

    [PROG] refont text + répète V1

--- Assets/Script/UI/UiTextDialogueOnGround.cs
+++ Assets/Script/UI/UiTextDialogueOnGround.cs
@@ -6,45 +6,20 @@ using UnityEngine;
 public class UiTextDialogueOnGround : MonoBehaviour
 {
     public ScriptableDialogue dialogue;
-    [SerializeField] AudioSource Source;
-    Coroutine _coroutine;
-    TextDialogueManager _textManager;
 
     private void OnTriggerEnter(Collider other)
     {
-        _textManager = other.GetComponentInChildren<TextDialogueManager>();
+        CharaMove chara = other.GetComponent<CharaMove>();
 
-        if (other.GetComponent<CharaMove>() != null && _textManager != null && _coroutine == null)
+        if (chara != null)
         {
-            _textManager.UiText.gameObject.SetActive(true);
-            _textManager.UiBack.gameObject.SetActive(true);
+            UiTextDialogueSpeaker speaker = chara.GetComponentInChildren<UiTextDialogueSpeaker>();
 
-            _coroutine = StartCoroutine(LaunchDialogue(Source, _textManager.UiText, 0));
-        }
-    }
-
-    IEnumerator LaunchDialogue(AudioSource source, TextMeshProUGUI text, int i)
-    {
-        dialogue.PlayDialogue(source, text, i);
-
-        yield return new WaitForSeconds(dialogue.Voice[i].length + 0.1f);
-
-        i++;
-
-        if (i < dialogue.Voice.Length || i < dialogue.Text.Length)
-        {
-            StartCoroutine(LaunchDialogue(source, text, i));
-        }
-        else
-        {
-            _coroutine = null;
-            i = 0;
-
-            if (_textManager != null)
+            if (speaker != null && speaker.Coroutine == null)
             {
-                _textManager.UiText.gameObject.SetActive(false);
-                _textManager.UiBack.gameObject.SetActive(false);
-                _textManager = null;
+                speaker.StartDialogue(dialogue);
+
+                Destroy(this.gameObject);
             }
         }
     }
