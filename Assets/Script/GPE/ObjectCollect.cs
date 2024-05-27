9d82947ffd6d7db275a002026382aae0abf3277b
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-05-27 16:10:46 +0200

    build

--- Assets/Script/GPE/ObjectCollect.cs
+++ Assets/Script/GPE/ObjectCollect.cs
@@ -3,7 +3,6 @@ using UnityEngine.UIElements;
 
 public class ObjectCollect : MonoBehaviour
 {
-    public Material MaterialOutline;
     public ScriptableDialogue DialogueBabyReccup;
     public bool IsPortable;
     public bool isListened;
